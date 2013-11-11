6d6f5bd7ed85ebf63af69288afecbf6374951502
Peter Nycander <peter.nycander@gmail.com>
2013-11-11 15:16:31 +0100

    removed unnecessary field in constructors

--- src/kth/game/othello/player/ComputerPlayer.java
+++ src/kth/game/othello/player/ComputerPlayer.java
@@ -2,8 +2,8 @@ package kth.game.othello.player;
 
 class ComputerPlayer extends AbstractPlayer {
 
-	public ComputerPlayer(String id, String name, Type type) {
-		super(id, name, type);
+	public ComputerPlayer(String id, String name) {
+		super(id, name, Player.Type.COMPUTER);
 	}
 
 }
