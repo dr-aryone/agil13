a3adf75fbbfcc6118d6c1e7f3736fb493aa3d53a
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-11 15:30:14 +0100

    added some test skeleton

---
+++ systemtest/kth/game/othello/TestBoardCreation.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+import kth.game.othello.*;
+import kth.game.othello.board.Node;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class TestBoardCreation extends TestCase {
+
+	@Test
+	public void testBoardCreation() {
+	}
+}
