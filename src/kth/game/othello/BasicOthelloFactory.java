3fc4f9585dfdb1e67dddcc167af51c415f55ab4c
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-11 15:48:21 +0100

    added stub classes for Othello and OthelloFactory

---
+++ src/kth/game/othello/BasicOthelloFactory.java
@@ -0,0 +1,23 @@
+package kth.game.othello;
+
+class BasicOthelloFactory implements OthelloFactory {
+
+	@Override
+	public Othello createComputerGameOnClassicalBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Othello createHumanGameOnOriginalBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Othello createHumanVersusComputerGameOnOriginalBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
