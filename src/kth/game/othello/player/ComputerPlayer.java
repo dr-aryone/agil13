99af9811baa0bf7d3836b1c4eadc9966c46ea3ab
Peter Nycander <peter.nycander@gmail.com>
2013-11-11 15:14:15 +0100

    Fixed compilation errors

--- src/kth/game/othello/player/ComputerPlayer.java
+++ src/kth/game/othello/player/ComputerPlayer.java
@@ -2,4 +2,8 @@ package kth.game.othello.player;
 
 class ComputerPlayer extends AbstractPlayer {
 
+	public ComputerPlayer(String id, String name, Type type) {
+		super(id, name, type);
+	}
+
 }
--- src/kth/game/othello/player/HumanPlayer.java
+++ src/kth/game/othello/player/HumanPlayer.java
@@ -2,4 +2,8 @@ package kth.game.othello.player;
 
 class HumanPlayer extends AbstractPlayer {
 
+	public HumanPlayer(String id, String name, Type type) {
+		super(id, name, type);
+	}
+
 }
