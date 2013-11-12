d601549475ebbacc3936504f43643c874df189dc
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-12 12:30:52 +0100

    added a test for board sorting

--- systemtest/kth/game/othello/test/BoardTest.java
+++ systemtest/kth/game/othello/test/BoardTest.java
@@ -2,7 +2,13 @@ package kth.game.othello.test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
+import kth.game.othello.board.BasicBoard;
+import kth.game.othello.board.Board;
 import kth.game.othello.board.Node;
 
 import org.junit.Test;
@@ -11,6 +17,8 @@ public class BoardTest extends TestCase {
 
 	private Node createMockedNode(int x, int y) {
 		Node node = mock(Node.class);
+		when(node.getId()).thenReturn("node" + x + "," + y);
+		when(node.getOccupantPlayerId()).thenReturn("player1");
 		when(node.getXCoordinate()).thenReturn(x);
 		when(node.getYCoordinate()).thenReturn(y);
 		return node;
@@ -18,6 +26,21 @@ public class BoardTest extends TestCase {
 
 	@Test
 	public void testBoardCreation() {
+		List<Node> boardNodes = new ArrayList<>();
+		List<Node> sortedNodes = new ArrayList<>();
 
+		for (int node = 0; node < 10; node++) {
+			boardNodes.add(createMockedNode(9 - node, 9 - node));
+			sortedNodes.add(createMockedNode(node, node));
+		}
+		Board board = new BasicBoard(boardNodes);
+		boardNodes = board.getNodes();
+		for (int node = 0; node < 10; node++) {
+			Node boardNode = boardNodes.get(node);
+			Node sortedNode = sortedNodes.get(node);
+			assertEquals(sortedNode.getXCoordinate(), boardNode.getXCoordinate());
+			assertEquals(sortedNode.getYCoordinate(), boardNode.getYCoordinate());
+			assertEquals(sortedNode.getId(), boardNode.getId());
+		}
 	}
 }
