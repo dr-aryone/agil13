a903046266fbbcc4680e5c6aad646e1eb80e116d
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-14 14:05:53 +0100

    added some javadoc to the constants interface

--- src/kth/game/othello/OthelloConstants.java
+++ src/kth/game/othello/OthelloConstants.java
@@ -1,7 +1,13 @@
 package kth.game.othello;
 
+/**
+ * An interface containing constants for the Othello board game.
+ */
 public interface OthelloConstants {
 
+	/**
+	 * The length of the quadratic sides of an Othello board, in squares.
+	 */
 	public static final int OTHELLO_BOARD_SIDE_LENGTH = 8;
 
 }
