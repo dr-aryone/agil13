52cf79851bf7852abc87c3d2dae880eb9f0eb31a
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-25 15:19:35 +0100

    added a simple observer test for BasicNode

--- test/kth/game/othello/board/NodeTest.java
+++ test/kth/game/othello/board/NodeTest.java
@@ -5,18 +5,21 @@ import java.util.Observer;
 
 import kth.game.othello.MockingBase;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NodeTest extends MockingBase {
 
 	@Test
-	public void testObserver() {
-		Node node = new BasicNode(0, 0);
+	public void testSwapObserving() {
+		final BasicNode node = new BasicNode(0, 0);
 		node.addObserver(new Observer() {
 			@Override
 			public void update(Observable o, Object arg) {
-				// TODO Auto-generated method stub
+				Assert.assertEquals(node, o);
+				Assert.assertEquals("p1", arg);
 			}
 		});
+		node.setOccupantPlayerId("p1");
 	}
 }
