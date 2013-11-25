e7f4d8f3da6ce512bab409ee9865ba83108bed2a
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-25 16:04:49 +0100

    adds more unit test code for score

--- test/kth/game/othello/score/ScoreTest.java
+++ test/kth/game/othello/score/ScoreTest.java
@@ -11,12 +11,13 @@ import kth.game.othello.player.Player;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class ScoreTest extends MockingBase {
 
 	@Test
 	public void testScoreObservation() {
-		List<Player> players = createMockedPlayers(3);
+		List<Player> players = createMockedPlayers(1);
 		BasicScore score = new BasicScore(players);
 		final AtomicBoolean updateTriggered = new AtomicBoolean(false);
 
@@ -29,8 +30,22 @@ public class ScoreTest extends MockingBase {
 			}
 		});
 
-		score.increasePoints("0", 123);
+		score.increasePoints("0", Mockito.anyInt());
 		if (!updateTriggered.get())
 			Assert.fail();
+	}
+
+	@Test
+	public void testScoreChange() {
+		List<Player> players = createMockedPlayers(3);
+		BasicScore score = new BasicScore(players);
+
+		Assert.assertEquals(0, score.getPoints("0"));
+
+		score.increasePoints("0", 10);
+		Assert.assertEquals(10, score.getPoints("0"));
+
+		score.decreasePoints("0", 20);
+		Assert.assertEquals(-10, score.getPoints("0"));
 	}
 }
