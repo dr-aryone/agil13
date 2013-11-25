e114be13fb6da82365c282addb3c3c96d081791b
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-11-25 15:22:17 +0100

    adds observation performed check in tests for BasicNode

--- test/kth/game/othello/board/NodeTest.java
+++ test/kth/game/othello/board/NodeTest.java
@@ -2,6 +2,7 @@ package kth.game.othello.board;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import kth.game.othello.MockingBase;
 
@@ -13,13 +14,17 @@ public class NodeTest extends MockingBase {
 	@Test
 	public void testSwapObserving() {
 		final BasicNode node = new BasicNode(0, 0);
+		final AtomicBoolean updateTriggered = new AtomicBoolean(false);
 		node.addObserver(new Observer() {
 			@Override
 			public void update(Observable o, Object arg) {
 				Assert.assertEquals(node, o);
 				Assert.assertEquals("p1", arg);
+				updateTriggered.set(true);
 			}
 		});
 		node.setOccupantPlayerId("p1");
+		if (!updateTriggered.get())
+			Assert.fail();
 	}
 }
