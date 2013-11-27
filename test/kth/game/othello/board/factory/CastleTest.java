306eafff7c55b6dc794ca2da4e47a70a6f1bce47
Peter Nycander <peter.nycander@gmail.com>
2013-11-27 15:59:11 +0100

    Added duplicate node check test to caste

--- test/kth/game/othello/board/factory/CastleTest.java
+++ test/kth/game/othello/board/factory/CastleTest.java
@@ -1,6 +1,10 @@
 package kth.game.othello.board.factory;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import kth.game.othello.MockingBase;
+import kth.game.othello.board.BasicBoardCreator;
+import kth.game.othello.board.Board;
 import kth.game.othello.board.BoardCreator;
 import kth.game.othello.board.Node;
 import kth.game.othello.board.NodeCreator;
@@ -36,5 +40,35 @@ public class CastleTest extends MockingBase {
 		castle.getBoard(createMockedPlayers(2));
 
 		Mockito.verify(boardCreator).createBoard(Mockito.anyListOf(Node.class));
+	}
+
+	@Test
+	public void testBoardContainsNoDuplicates() {
+		NodeCreator nodeCreator = mock(NodeCreator.class);
+		int boardSize = 7;
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				Node mockedNode = createMockedNode(i, j);
+				when(nodeCreator.createNodeWithCoordinate(i, j)).thenReturn(mockedNode);
+				mockedNode = createMockedNode(i, j, i + "," + j, "playerId");
+				when(nodeCreator.createNodeWithCoordinateAndOccupant(Mockito.eq(i), Mockito.eq(j), Mockito.anyString()))
+						.thenReturn(mockedNode);
+			}
+		}
+
+		Diamond diamond = new Diamond(nodeCreator, new BasicBoardCreator());
+		Board board = diamond.getDiamondBoard(createMockedPlayers(3), boardSize);
+
+		for (int i = 0; i < board.getNodes().size(); i++) {
+			for (int j = 0; j < board.getNodes().size(); j++) {
+				if (i == j)
+					continue;
+
+				Node node = board.getNodes().get(i);
+				Node compareNode = board.getNodes().get(j);
+				Assert.assertFalse((node.getXCoordinate() == compareNode.getXCoordinate() && node.getYCoordinate() == compareNode
+						.getYCoordinate()));
+			}
+		}
 	}
 }
