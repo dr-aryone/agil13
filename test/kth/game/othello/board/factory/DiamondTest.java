2d912ef9a832c23cb4e27a6a4fd0b853a6bbb658
Peter Nycander <peter.nycander@gmail.com>
2013-11-27 15:34:44 +0100

    Adds a test case which breaks the given diamond board

--- test/kth/game/othello/board/factory/DiamondTest.java
+++ test/kth/game/othello/board/factory/DiamondTest.java
@@ -1,6 +1,10 @@
 package kth.game.othello.board.factory;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import kth.game.othello.MockingBase;
+import kth.game.othello.board.BasicBoardCreator;
+import kth.game.othello.board.Board;
 import kth.game.othello.board.BoardCreator;
 import kth.game.othello.board.Node;
 import kth.game.othello.board.NodeCreator;
@@ -29,9 +33,9 @@ public class DiamondTest extends MockingBase {
 
 	@Test
 	public void testThatABoardIsCreatedIfTheNumberOfPlayersAreThree() {
-		NodeCreator nodeCreator = Mockito.mock(NodeCreator.class);
+		NodeCreator nodeCreator = mock(NodeCreator.class);
 		Mockito.when(nodeCreator.createNodeWithCoordinate(Mockito.anyInt(), Mockito.anyInt())).thenReturn(null);
-		BoardCreator boardCreator = Mockito.mock(BoardCreator.class);
+		BoardCreator boardCreator = mock(BoardCreator.class);
 		Mockito.when(boardCreator.createBoard(Mockito.anyListOf(Node.class))).thenReturn(null);
 		Diamond diamond = new Diamond(nodeCreator, boardCreator);
 		int boardSize = 7;
@@ -39,5 +43,30 @@ public class DiamondTest extends MockingBase {
 		diamond.getDiamondBoard(createMockedPlayers(3), boardSize);
 
 		Mockito.verify(boardCreator).createBoard(Mockito.anyListOf(Node.class));
+	}
+
+	@Test
+	public void testBoardContainsNoDuplicates() {
+		NodeCreator nodeCreator = mock(NodeCreator.class);
+		int boardSize = 7;
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				Node mockedNode = createMockedNode(i, j);
+				when(nodeCreator.createNodeWithCoordinate(i, j)).thenReturn(mockedNode);
+				mockedNode = createMockedNode(i, j, i + "," + j, "playerId");
+				when(nodeCreator.createNodeWithCoordinateAndOccupant(Mockito.eq(i), Mockito.eq(j), Mockito.anyString()))
+						.thenReturn(mockedNode);
+			}
+		}
+
+		Diamond diamond = new Diamond(nodeCreator, new BasicBoardCreator());
+		Board board = diamond.getDiamondBoard(createMockedPlayers(3), boardSize);
+
+		for (Node n : board.getNodes()) {
+			for (Node m : board.getNodes()) {
+				Assert.assertFalse((n.getXCoordinate() == m.getXCoordinate() && n.getYCoordinate() == m
+						.getYCoordinate()));
+			}
+		}
 	}
 }
