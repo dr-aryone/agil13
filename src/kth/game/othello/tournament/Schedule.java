022d957e868913c23856bbdbcb881550bc96876e
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-12-05 18:41:40 +0100

    implements a simple Schedule

--- src/kth/game/othello/tournament/Schedule.java
+++ src/kth/game/othello/tournament/Schedule.java
@@ -1,7 +1,23 @@
 package kth.game.othello.tournament;
 
-public class Schedule {
+import java.util.Deque;
+import java.util.LinkedList;
 
+class Schedule {
+
+	private final Deque<MatchUp> matchUps = new LinkedList<>();
+
+	void addMatchUp(MatchUp matchUp) {
+		matchUps.add(matchUp);
+	}
+
+	MatchUp nextMatchUp() {
+		return matchUps.poll();
+	}
+
+	boolean isOver() {
+		return matchUps.isEmpty();
+	}
 	// contains a a list of match-ups and their order
 
 	// hopefully some other pretty functionality too...
