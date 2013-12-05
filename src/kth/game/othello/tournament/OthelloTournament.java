996f1167fe4c9ebe81445bfebf7e1f6e37720334
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-12-05 18:52:11 +0100

    adds some creation code to the OthelloTournament

--- src/kth/game/othello/tournament/OthelloTournament.java
+++ src/kth/game/othello/tournament/OthelloTournament.java
@@ -1,6 +1,42 @@
 package kth.game.othello.tournament;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kth.game.othello.player.BasicPlayerCreator;
+import kth.game.othello.player.Player;
+import kth.game.othello.player.PlayerCreator;
+import kth.game.othello.player.movestrategy.MoveStrategy;
+
 public class OthelloTournament {
 
+	private final ResultDisplay resultDisplay;
+	private final Schedule schedule;
+
+	OthelloTournament(ResultDisplay resultDisplay) {
+		this(resultDisplay, new Schedule());
+	}
+
 	// contains a schedule and a way to display the results
+
+	OthelloTournament(ResultDisplay resultDisplay, Schedule schedule) {
+		this.resultDisplay = resultDisplay;
+		this.schedule = schedule;
+	}
+
+	public static OthelloTournament tournamentFromPlayers(List<Player> players) {
+		// TODO
+		return null;
+	}
+
+	public static OthelloTournament tournamentFromMoveStrategies(List<MoveStrategy> strategies) {
+		List<Player> players = new ArrayList<>();
+		PlayerCreator playerCreator = new BasicPlayerCreator();
+		for (int i = 0; i < strategies.size(); i++) {
+			MoveStrategy moveStrategy = strategies.get(i);
+			String playerName = String.format("Player %d", i);
+			players.add(playerCreator.createComputerPlayer(playerName, moveStrategy));
+		}
+		return tournamentFromPlayers(players);
+	}
 }
