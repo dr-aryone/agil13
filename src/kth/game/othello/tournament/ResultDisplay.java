b0f681bf140a6df122cdc43cac44c638af51519e
Peter Nycander <peter.nycander@gmail.com>
2013-12-07 11:46:39 +0100

    Removes ResultDisplay from old location

--- src/kth/game/othello/tournament/ResultDisplay.java
+++
@@ -1,8 +0,0 @@
-package kth.game.othello.tournament;
-
-public interface ResultDisplay {
-
-	// defines in what way we can display the results of an othello tournament
-
-	void displayResults(Results results);
-}
