c620ca170ebe6925a31475a15ea5c111bdfa27ba
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-12-13 17:12:14 +0100

    fixes failing test for peter nycanders stupid code

--- MSS_othello/test/test/othello/score/ScoreObserver.java
+++ MSS_othello/test/test/othello/score/ScoreObserver.java
@@ -4,6 +4,7 @@ import game.othello.board.StandardNode;
 import game.othello.score.Score_impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -46,6 +47,43 @@ public class ScoreObserver {
 
 		Assert.assertTrue(list.contains(playerId));
 		Assert.assertTrue(list.contains(mockedNode.getOccupantPlayerId()));
+	}
+
+	@Test
+	public void testDoubleScoreAtBoundaries() {
+		Player p1 = Mockito.mock(Player.class);
+		Player p2 = Mockito.mock(Player.class);
+
+		Mockito.when(p1.getId()).thenReturn("1");
+		Mockito.when(p2.getId()).thenReturn("2");
+
+		Board board = Mockito.mock(Board.class);
+		StandardNode boundarieNode = Mockito.mock(StandardNode.class);
+		Mockito.when(boundarieNode.getXCoordinate()).thenReturn(0);
+		Mockito.when(boundarieNode.getYCoordinate()).thenReturn(0);
+		Mockito.when(boundarieNode.getOccupantPlayerId()).thenReturn("1");
+
+		StandardNode middleNode = Mockito.mock(StandardNode.class);
+		Mockito.when(middleNode.getXCoordinate()).thenReturn(1);
+		Mockito.when(middleNode.getYCoordinate()).thenReturn(1);
+		Mockito.when(middleNode.getOccupantPlayerId()).thenReturn("2");
+
+		Mockito.when(board.hasNode(0, 0)).thenReturn(true);
+		Mockito.when(board.hasNode(0, 1)).thenReturn(true);
+		Mockito.when(board.hasNode(0, 2)).thenReturn(true);
+		Mockito.when(board.hasNode(1, 0)).thenReturn(true);
+		Mockito.when(board.hasNode(1, 1)).thenReturn(true);
+		Mockito.when(board.hasNode(1, 2)).thenReturn(true);
+		Mockito.when(board.hasNode(2, 0)).thenReturn(true);
+		Mockito.when(board.hasNode(2, 1)).thenReturn(true);
+		Mockito.when(board.hasNode(2, 2)).thenReturn(true);
+		Score_impl score = new Score_impl(board);
+		score.addPlayers(Arrays.asList(p1, p2));
+
+		score.update(middleNode, null);
+		Assert.assertEquals(1, score.getPoints("2"));
+		score.update(boundarieNode, null);
+		Assert.assertEquals(2, score.getPoints("1"));
 	}
 
 	private class TestObserver implements Observer {
